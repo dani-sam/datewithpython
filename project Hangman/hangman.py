82af00dd975e3c5d7c8cda407dd9aaa4e1096b07
dani-sam <samdani295@gmail.com>
2023-09-03 22:43:30 +0530

    created empty list,insert '-' add on it.then added crosscheck it,interchanged position of display with help of loop

--- project Hangman/hangman.py
+++ project Hangman/hangman.py
@@ -1,9 +1,20 @@
 import random
 word_list = ["camel","austria","football"]
 chosen_word = random.choice(word_list)
+# len_chosenWord = len(chosen_word)
+# print(len_chosenWord)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+symbol = '_'
+for _ in range(len(chosen_word)):
+    display  += '_'
+print(display) 
+
 guess = input("Guess a word:").lower()
-for letter in chosen_word:
-    if guess == letter:
-        print("Right")
-    else:
-        print("Wrong")
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = guess
+print(display)
